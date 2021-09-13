e043afe880355a68f09ee27f388913d7fed891ff
Minggui <smg@nwafu.edu.cn>
2021-09-13 10:39:45 +0800

    Test

---
+++ 2021_Sep/Sep_13rd_447.cpp
@@ -0,0 +1,19 @@
+//
+// Created by smg_nwafu on 2021/9/13.
+//
+#include <iostream>
+#include <vector>
+#include <dbg.h>
+
+using namespace std;
+
+class Solution{
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+
+    }
+};
+
+int main(){
+    dbg("test");
+}
---
+++ cmake-build-debug/CMakeFiles/LeetCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeetCode.dir/2021_Sep/Sep_13rd_447.cpp.o"
+  "LeetCode"
+  "LeetCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LeetCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
