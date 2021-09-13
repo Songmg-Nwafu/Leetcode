1f590756f9a5ec681000b48afe4fa2b2dbf3ba7d
Minggui <smg@nwafu.edu.cn>
2021-09-13 11:20:39 +0800

    2021-9-13 回旋镖的数量

--- 2021_Sep/Sep_13rd_447.cpp
+++ 2021_Sep/Sep_13rd_447.cpp
@@ -3,17 +3,31 @@
 //
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <dbg.h>
 
 using namespace std;
-
+/**
+ * 447. 回旋镖的数量(中等)
+ */
 class Solution{
 public:
     int numberOfBoomerangs(vector<vector<int>>& points) {
-
+        int count = 0;
+        unordered_map<int,int> map;
+        for (auto& p : points) {
+            map.clear();
+            for(auto& q : points){
+                int dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                count += 2 * map[dis]++;
+            }
+        }
+        return count;
     }
 };
 
 int main(){
-    dbg("test");
+    Solution solution;
+    vector<vector<int>> points = {{0,0},{1,0},{2,0}};
+    dbg(solution.numberOfBoomerangs(points));
 }
